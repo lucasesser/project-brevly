2fd71727385f708f5f035dc32eb6b599edda3358
lucasesser <lucas2002esser@gmail.com>
2026-03-27 03:15:07 -0400

    Pre configurado drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'drizzle-kit'
+import { env } from './src/env'
+
+export default defineConfig({
+    out: './src/infra/db/migrations',
+    schema: './src/infra/db/schemas',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: env.DATABASE_URL
+    }
+})
---
+++ src/infra/db/index.ts
@@ -0,0 +1,5 @@
+import { drizzle } from "drizzle-orm/postgres-js"
+import { env } from "../../env";
+
+const db = drizzle(env.DATABASE_URL);
+const result = db.execute('select 1')
