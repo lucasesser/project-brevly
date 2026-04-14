9053c9a3a4f5b25e6ef470909b2764f35d408eae
lucasesser <lucas2002esser@gmail.com>
2026-04-14 01:38:34 -0400

    add incremente count

--- src/infra/db/schemas/links.ts
+++ src/infra/db/schemas/links.ts
@@ -1,8 +1,9 @@
-import { pgTable, text} from "drizzle-orm/pg-core";
+import { pgTable, text, integer} from "drizzle-orm/pg-core";
 import { uuidv7 } from "uuidv7";
 
 export const links = pgTable("links", {
   id: text('id').primaryKey().$defaultFn(() => uuidv7()),
   linkOriginal: text('linkOriginal').notNull(),
-  linkEncurtado: text('linkEncurtado').notNull().unique()
+  linkEncurtado: text('linkEncurtado').notNull().unique(),
+  accessCount: integer('accessCount').notNull().default(0)
 });
